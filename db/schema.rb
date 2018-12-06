a247ad17bb2cab1e42dfeb358ccc9687fc3c1aba
tatsuya.tsutusmi <tatsuya223@gmail.com>
2018-12-06 08:33:31 +0000

    correction20181206

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181202131000) do
+ActiveRecord::Schema.define(version: 20181205165925) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
